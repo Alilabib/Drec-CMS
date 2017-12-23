f19d32f7953d9f259b5e8259320ac60f19521e95
Alilabib <alilabib65@gmail.com>
2017-12-23 13:54:49 +0200

    CMS Project

---
+++ includes/head.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <meta charset="utf-8">
+    <title>HTML5=>Training</title>
+    <link rel="stylesheet" href="css/style.css" >
+        <!--[if lt IE 9] >
+                <script src="html5shiv.min.js"></script>
+        <![endif]-->
+    </head>
+    <body dir="rtl">
+        <!-- Start Header -->
+        <header class="main">
+            <h1> بوابة وزارة العدل  </h1>
+            
+        </header>
+        <!-- End Header-->
+        
+        <!-- Start Nav  -->
+    	<nav> 
+                <ul> 
+                    <li><a href=""> بوابة وزارة العدل </a> </li>
+                    <li><a href=""> أخبار الإدارة    </a> </li>
+                    <li><a href=""> أنشطة ومهارات  </a> </li>
+                    <li><a href=""> إجتماعيات     </a> </li>
+                    <li><a href=""> لوحة الشرف    </a> </li>
+                    <li><a href=""> لوحة الإعلانات  </a> </li>
+                    <li><a href=""> خريطة الموقع   </a> </li>
+                    <li><a href=""> إتصل بنا      </a> </li>
+                    <li ><a href="sigin.html" class="btn-login"> تسجيل الدخول   </a> </li>
+                    <li ><a href="signup1.html" class="btn-sign"> مستخدم جديد    </a></li>
+                    
+                </ul>
+            
+            
+        </nav>
+        <!-- End NAv -->
+        
---
+++ index.php
@@ -0,0 +1,112 @@
+<?php include'includes/head.php' ?>
+
+
+        <!-- Start main Content --> 
+        
+        <main> 
+         خدمات الشهر العقاري و التوثيق  
+        </main>
+        
+        <!-- End Main Content -->
+        
+        <!-- Start Artical -->
+        <div class="post">      
+            <article>  
+            
+            
+            
+            <img src="images/c54bc791-e5b0-49b8-b272-ad761ef78831.jpg">
+            
+            </article>
+            
+            <article id="head"> 
+                <header>  
+                    <h3> خدمات رسوم تسجيل </h3>
+                    
+                </header>
+                <section>
+                    <p>جميع انواع الطلبات بمأمورية الشهر الشخصى والمستندات المطلوبة والرسوم المقررة
+                        <a href="regstration.html"> المزيد</a></p>
+                   
+                </section>    
+        </article>
+        
+        <article> 
+                <header>  
+                    <h3> خدمات إطلاع  </h3>
+                
+                </header> 
+                <section> 
+                     <p>دورة عمل الشهر الشخصى بالمكتب:
+                        <a href="office.html"> المزيد</a></p>
+                    
+                </section>    
+        </article>
+        
+        <article> 
+                <header>  
+                    <h3> خدمات إستعلام </h3>
+    
+                </header> 
+                <section> 
+                    <p>دورة عمل الشهر الشخصى بالمأمورية:
+                        <a href="personal.html"> المزيد</a></p>
+                    
+                </section>    
+        </article>
+            
+        <article> 
+                <header>  
+                    <h3> خدمات حصول </h3>
+    
+                </header> 
+                <section> 
+                    <p>
+                        صيغة طلب الشهادة العقارية وتوقيت التسليم <br>
+                        توقيت تسليم الشهادة: بعد 15 يوم
+                     <a href="form.html"> المزيد</a>   
+                    </p>
+                    
+                </section>    
+        </article>    
+        </div>
+        <!-- End Artical -->
+        
+        <!-- Start side bar -->
+        
+        <aside> 
+            <section class="links">
+            <h3> القوائم  </h3>
+              <ul> 
+                    <li> <a href="index.html"> الرئيسية         </a> </li>
+                    <li> <a href=""> عن الإدارة       </a> </li>
+                    <li> <a href=""> مساعد الوزير     </a> </li>
+                    <li> <a href=""> الهيكل التنظيمي    </a> </li>
+                    <li> <a href=""> المكتب الفني      </a> </li>
+                    <li> <a href=""> رؤية الإدارة      </a> </li>
+                    <li> <a href=""> إختصاصات الإدارة  </a> </li>
+                    <li> <a href=""> خدمات الجمهور    </a> </li>
+                    <li> <a href=""> الشكاوي والإقتراحات </a> </li>
+                    <li> <a href=""> الإنجازات        </a> </li>
+                </ul>
+            </section>    
+            
+        </aside>
+        
+        <!-- END  side bar  -->
+        
+        <!-- Start Clear fix --> 
+                
+            <div class="clear-fix"></div>
+        
+        <!-- End   Clear fix -->
+        
+        <!-- Start Footer -->
+        
+        <footer> 
+            Copyrights 2017 &copy; All Rights Recived                  
+        </footer>
+            
+        <!-- End Footer -->
+    </body>
+</html>
